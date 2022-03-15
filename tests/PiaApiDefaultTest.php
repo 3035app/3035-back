72d190438f5c7656054f636d57c472ee20fe1493
paoDweb <info@paoDweb.com>
2022-03-15 10:43:12 +0100

    fix: update psql version

--- tests/PiaApiDefaultTest.php
+++ tests/PiaApiDefaultTest.php
@@ -19,92 +19,123 @@ use Symfony\Component\Console\Output\NullOutput;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\KernelInterface;
 use PiaApi\Entity\Oauth\User;
+use PiaApi\Entity\Pia\Structure;
 use PiaApi\Services\ApplicationService;
-use PiaApi\Repository\ApplicationRepository;
-
+use PiaApi\Repository\StructureRepository;
+use Doctrine\Persistence\ObjectManager;
+use PiaApi\Services\StructureService;
+use PiaApi\Services\StructureTypeService;
+use PiaApi\Entity\Oauth\Client;
+use PiaApi\Services\UserService;
 
 class PiaApiDefaultTest extends WebTestCase
 {
-   public function testInitializeEnv()
+   private $application;
+   private $manager;
+   private $structure;
+   private $users;
+
+   public function testInitializeFixture()
    {
       # Arrange
       self::bootKernel();
-      $container = self::$container;
-      $this->piaApplication($container);
+      $this->manager = self::$container->get('doctrine')->getManager();
+      $this->application = $this->piaApplication(self::$container);
+      $this->structure = $this->piaStructure(self::$container);
+      $this->users = $this->piaUsers(self::$container);
 
       # Act
+      // $this->manager->refresh($structure);
       // $repository->add($testWeatherQuery1);
 
       # Assert
-      $this->assertEquals(2, 1+1);
+      # users array is on the form: [[<user:id> => <user:username>], ...]
+      $this->assertEquals(count($this->_users()), count($this->users));
    }
 
-   public function testsAreWorkingTest ()
+   public function testHello ()
    {
       print_r('hello');
       $this->assertEquals(2, 1+1);
    }
 
-   public function piaApplication($container)
+   public function piaApplication($container): Client
    {
       // Arrange
       $applicationService = $container->get(ApplicationService::class);
-      $repository = $container->get(ApplicationRepository::class);
 
       // Act
-      $application = $applicationService->createApplication('Test-app', '');
+      $application = $this->manager
+            ->getRepository(Client::class)
+            ->findOneByName('Test-app')
+            ;
+      if (null == $application) {
+         $application = $applicationService->createApplication('Test-app', '');
+         $this->manager->persist($application);
+         $this->manager->flush();
+      }
 
       // Assert
-      $records = $repository->findAll();
       $this->assertEquals('Test-app', $application->getName());
       $this->assertEquals('', $application->getUrl());
+      return $application;
    }
 
-   public function piaStructure(KernelInterface $kernel): Response
+   public function piaStructure($container): Structure
    {
-      $application = new Application($kernel);
-      $application->setAutoExit(false);
-      $input = new ArrayInput([
-         'command' => 'pia:structure:create',
-         'name' => 'Test-struct',
-         '--type' => 'Test-St1-type',
-         ]);
-      $output = new NullOutput();
-      $application->run($input, $output);
-      return new Response('');
-   }
+      // Arrange
+      $structureService = $container->get(StructureService::class);
+      $structureTypeService = $container->get(StructureTypeService::class);
 
-   public function piaUsers(KernelInterface $kernel): Response
-   {
-      $application = new Application($kernel);
-      $application->setAutoExit(false);
-      foreach ($this->_users() as $user) {
-         $input = new ArrayInput([
-            'command' => 'pia:user:create',
-            'email' => $user['email'],
-            'password' => $user['password'],
-            '--structure' => $user['structure'],
-            ]);
-         $output = new NullOutput();
-         $application->run($input, $output);
+      // Act
+      $structure = $this->manager
+            ->getRepository(Structure::class)
+            ->findOneByName('Test-struct')
+            ;
+      if (null == $structure) {
+         $structureType = $structureTypeService->createStructureType('Test-St1-type');
+         $structure = $structureService->createStructure('Test-struct', $structureType);
+         $this->manager->persist($structureType);
+         $this->manager->persist($structure);
+         $this->manager->flush();
       }
-      return new Response('');
+
+      // Assert
+      $this->assertEquals('Test-struct', $structure->getName());
+      $this->assertEquals('Test-St1-type', $structure->getType()->getName());
+      return $structure;
    }
 
-   public function piaRoleUsers(KernelInterface $kernel): Response
+   public function piaUsers($container): array
    {
-      $application = new Application($kernel);
-      $application->setAutoExit(false);
-      foreach ($this->_users() as $user) {
-         $input = new ArrayInput([
-            'command' => 'pia:user:promote',
-            'email' => $user['email'],
-            '--role' => $user['role'],
-            ]);
-         $output = new NullOutput();
-         $application->run($input, $output);
+      // Arrange
+      $userService = $container->get(UserService::class);
+
+      // Act
+      $users = [];
+      foreach ($this->_users() as $u) {
+         $user = $this->manager
+            ->getRepository(User::class)
+            ->findOneByEmail($u['email'])
+            ;
+         if (null == $user) {
+            $user = $userService->createUser(
+               $u['email'],
+               $u['password'],
+               $this->structure,
+               $this->application
+               );
+            $user->addRole($u['role']);
+            $this->manager->persist($user);
+            $this->manager->flush();
+         }
+
+         // Assert
+         $this->assertEquals($u['email'], $user->getEmail());
+         $this->assertTrue(in_array($u['role'], $user->getRoles()));
+         $users[$user->getId()] = $user->getUsername();
       }
-      return new Response('');
+      return $users;
    }
 
    private function _users(): array
