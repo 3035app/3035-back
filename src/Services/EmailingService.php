85477b08af1bc427f9b7fd331e15902382e71ecd
paoDweb <info@paoDweb.com>
2022-03-29 14:54:07 +0200

    notify evaluator/dpo

---
+++ src/Services/EmailingService.php
@@ -0,0 +1,142 @@
+<?php
+
+/*
+ * Copyright (C) 2015-2018 Libre Informatique
+ *
+ * This file is licensed under the GNU LGPL v3.
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+namespace PiaApi\Services;
+
+use FOS\UserBundle\Mailer\MailerInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class EmailingService
+{
+    protected $environment;
+    protected $from;
+    protected $logger;
+    protected $mailer;
+    protected $router;
+    protected $twig;
+
+    public function __construct(
+        \Twig_Environment $twig,
+        LoggerInterface $logger,
+        MailerInterface $mailer,
+        UrlGeneratorInterface $router,
+        string $PialabEnvironment,
+        array $PialabFromEmail
+    ) {
+        $this->environment = $PialabEnvironment;
+        $this->from = $PialabFromEmail;
+        $this->logger = $logger;
+        $this->mailer = $mailer;
+        $this->router = $router;
+        $this->twig = $twig;
+    }
+
+    /**
+     * send email to evaluator when a redactor ask for evaluating a processing.
+     *
+     * @access public
+     * @param array
+     * @param string
+     * @param string
+     * @return int
+     */
+    public function notifyWhenAskingForProcessingEvaluation($processing, $recipientEmail, $recipientName)
+    {
+        list($name, $route, $routeAttr) = $processing;
+        $template = 'pia/Email/processing_evaluation%s.email.twig';
+        $params = ['processing_name' => $name, 'processing_url' => $this->getAbsoluteUrl($route, $routeAttr)];
+        $subject = $this->twig->render(sprintf($template, '_subject'), $params);
+        $body = $this->twig->render(sprintf($template, '_body'), $params);
+        $to = [$recipientEmail => $recipientName];
+        return $this->sendEmail($subject, $body, $this->from, $to);
+    }
+
+    /**
+     * send email to evaluator when a dpo emit opinion or observations on a processing.
+     *
+     * @access public
+     * @param array
+     * @param string
+     * @param string
+     * @return int
+     */
+    public function notifyWhenEmittingOpinionOrObservations($processing, $recipientEmail, $recipientName)
+    {
+        list($name, $route, $routeAttr) = $processing;
+        $template = 'pia/Email/emitting_opinion_or_observations%s.email.twig';
+        $params = ['processing_name' => $name, 'processing_url' => $this->getAbsoluteUrl($route, $routeAttr)];
+        $subject = $this->twig->render(sprintf($template, '_subject'), $params);
+        $body = $this->twig->render(sprintf($template, '_body'), $params);
+        $to = [$recipientEmail => $recipientName];
+        return $this->sendEmail($subject, $body, $this->from, $to);
+    }
+
+
+
+
+
+
+
+
+    /**
+     * @param string       $subject
+     * @param string       $body
+     * @param array|string $from
+     * @param array|string $to
+     * @return int
+     */
+    private function sendEmail($subject, $body, $from, $to)
+    {
+        // test environment: add string TEST to subject
+        if ($this->isTestEnvironment()) {
+            $subject = '## TEST ##' . $subject;
+        }
+
+        $email = (new \Swift_Message())
+            ->setSubject($subject)
+            ->setBody($body, 'text/html')
+            ->setFrom($from)
+            ->setTo($to)
+            ;
+
+        // dev environment: log email
+        if ($this->isDevEnvironment()) {
+            $this->logger->info('Emailing to ' . json_encode($to) . ' : ' . $subject . "\n" . $body);
+            return 0; // number of successful recipients reached
+        }
+
+        return $this->mailer->send($email);
+    }
+
+    /**
+     * @return bool
+     */
+    private function getAbsoluteUrl($route, $params)
+    {
+        return $this->router->generate($route, $params, UrlGeneratorInterface::ABSOLUTE_URL);
+    }
+
+    /**
+     * @return bool
+     */
+    private function isTestEnvironment()
+    {
+        return 'test' == $this->environment;
+    }
+
+    /**
+     * @return bool
+     */
+    private function isDevEnvironment()
+    {
+        return 'dev' == $this->environment;
+    }
+}
