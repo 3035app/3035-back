c37c7b63dbfdb93e2e873296c7fef848f2072fa5
paoDweb <info@paoDweb.com>
2022-04-19 15:46:34 +0200

    feat: add historical comments

---
+++ src/Entity/Pia/Traits/TrackingLogTrait.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace PiaApi\Entity\Pia\Traits;
+
+use Doctrine\Common\Persistence\Mapping\ClassMetadata;
+use Doctrine\Common\Persistence\ObjectManager;
+use PiaApi\Entity\Oauth\User;
+use PiaApi\Entity\Pia\TrackingLog;
+
+trait TrackingLogTrait
+{
+    /**
+     * Logs a tracking activity entry.
+     */
+    public function logTrackingActivity(User $user, string $activity)
+    {
+        assert(null != $this->getId(), 'entity must have been saved before logging an activity.');
+        return new TrackingLog($activity, $user, $this->getEntityClass(), $this->getId());
+    }
+
+    /**
+     * Returns the tracking log entries for the current object.
+     * 
+     * @JMS\VirtualProperty
+     * @JMS\SerializedName("trackings")
+     */
+    public function getTrackingLogs()
+    {
+        if (isset($this->entityManager) && null !== $this->entityManager)
+        {
+            # request optimised in repository
+            $options = ['contentType' => $this->getEntityClass(), 'entityId' => $this->getId()];
+            return $this->entityManager->getRepository(TrackingLog::class)->findTrackingsBy($options);
+        }
+        return [];
+    }
+
+    /**
+     * Retrieves entityManager injected by ObjectManagerAware interface.
+     * not retrieved in creation mode.
+     */
+    public function injectObjectManager(ObjectManager $objectManager, ClassMetadata $classMetadata)
+    {
+        $this->entityManager = $objectManager;
+    }
+
+    public function getEntityClass()
+    {
+        $classname = get_class($this);
+        if ($pos = strrpos($classname, '\\')) return substr($classname, $pos + 1);
+        return $pos;
+    }
+}
