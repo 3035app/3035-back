c33a61b9e1ae4dcf51c4f23cba29e06930c6599c
paoDweb <info@paoDweb.com>
2022-04-26 14:52:02 +0200

    feat: add comment to datatypes

--- src/Controller/Pia/ProcessingController.php
+++ src/Controller/Pia/ProcessingController.php
@@ -407,6 +407,11 @@ class ProcessingController extends RestController
         // before merging!
         $this->notifyOrTrack($request, $processing);
         $this->mergeFromRequest($processing, $updatableAttributes, $request);
+
+        // detect that fields to save comments
+$json = json_encode($processing->getInformedConcernedPeople());
+throw new AccessDeniedHttpException($json);
+
         $this->detachUsersAttachUsersNewPlace($processing, $start_point);
         $this->updateSupervisorsPia($request, $processing);
         $this->update($processing);
