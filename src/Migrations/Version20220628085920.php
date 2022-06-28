5a990aca94ce903eb99474856f001e64f3f7a885
Jean-Philippe Ouvrard <jp.ouvrard@lesoctetslibres.com>
2022-06-28 11:16:54 +0200

    fix(Pia): :bug: Generate migrations, add some initiales value to hiss opinion

---
+++ src/Migrations/Version20220628085920.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace DoctrineMigrations;
+
+use Doctrine\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+
+class Version20220628085920 extends AbstractMigration implements ContainerAwareInterface
+{
+    use ContainerAwareTrait;
+
+    public function up(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql("UPDATE pia set hiss_processing_implemented_status = 0 WHERE hiss_processing_implemented_status IS NULL");
+        $this->addSql('ALTER TABLE pia ALTER hiss_processing_implemented_status SET NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE pia ALTER hiss_processing_implemented_status DROP NOT NULL');
+    }
+}
